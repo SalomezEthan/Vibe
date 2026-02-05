7fe41980522d12f336f6a83b740fa3e9d18af2ba
Ethan Salomez <salomez-ethan@outlook.fr>
2026-02-05 23:36:22 +0100

    Ajout de l'entité SongEntity

---
+++ Vibe.Core.Tests/Song/SongEntity.Tests.cs
@@ -0,0 +1,35 @@
+﻿using Vibe.Core.Song;
+using Vibe.Core.Song.ValueObjects;
+
+namespace Vibe.Core.Tests.Song
+{
+    [TestClass]
+    public sealed class SongEntityTests
+    {
+        private SongEntity CreateTemplateEntity()
+        {
+            return new SongEntity(
+                new Guid(),
+                new SongName("Hello World :"),
+                [Guid.NewGuid()],
+                new SongDuration(TimeSpan.FromSeconds(30))
+                );
+        }
+
+        [TestMethod]
+        public void AddPerformer_ShouldThrow_WhenSongPerformersAlreadyContainsPerformer()
+        {
+            var song = CreateTemplateEntity();
+            Guid duplicatedId = song.Performers[0];
+            Assert.ThrowsExactly<ArgumentException>(() => song.AddPerformer(duplicatedId));
+        }
+
+        [TestMethod]
+        public void RemovePerformer_ShouldThrow_WhenSongPerformersHasNotPerformer()
+        {
+            var song = CreateTemplateEntity();
+            Guid randomId = Guid.NewGuid();
+            Assert.ThrowsExactly<ArgumentException>(() => song.RemovePerformer(randomId));
+        }
+    }
+}
