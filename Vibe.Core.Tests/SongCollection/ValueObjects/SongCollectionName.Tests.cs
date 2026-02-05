c06537b352d9c92fdfa38d7369dca88bb622823a
Ethan Salomez <salomez-ethan@outlook.fr>
2026-02-06 00:18:54 +0100

    Ajout du VO SongCollectionName

---
+++ Vibe.Core.Tests/SongCollection/ValueObjects/SongCollectionName.Tests.cs
@@ -0,0 +1,26 @@
+﻿using Vibe.Core.SongCollection.ValueObjects;
+
+namespace Vibe.Core.Tests.SongCollection.ValueObjects
+{
+    [TestClass]
+    public sealed class SongCollectionNameTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldThrow_WhenNameIsEmpty()
+        {
+            Assert.ThrowsExactly<ArgumentException>(() => new SongCollectionName(string.Empty));
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldThrow_WhenNameIsWhiteSpace()
+        {
+            Assert.ThrowsExactly<ArgumentException>(() => new SongCollectionName(new string(' ', 10)));
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldThrow_WhenNameIsTooLong()
+        {
+            Assert.ThrowsExactly<ArgumentOutOfRangeException>(() => new SongCollectionName(new string('c', 129)));
+        }
+    }
+}
---
+++ Vibe.Core/SongCollection/ValueObjects/SongCollectionName.cs
@@ -0,0 +1,43 @@
+﻿namespace Vibe.Core.SongCollection.ValueObjects
+{
+    public sealed class SongCollectionName
+    {
+        readonly string _name;
+
+        /// <summary>
+        /// Crée le nom d'une collection de sons à partir d'un <see cref="string"/>.
+        /// </summary>
+        /// <param name="name">Le nom de la colletion.</param>
+        /// <exception cref="ArgumentException">Un nom de collection de sons ne peut pas être vide.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Un nom de collection de sons ne peut dépasser 128 caractères.</exception>
+        public SongCollectionName(string name)
+        {
+            name = name.Trim();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Un nom de collection de sons ne peut pas être vide.");
+            }
+
+            if (name.Length > 128)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(name),
+                    name.Length,
+                    "Un nom de collection de sons ne peut dépasser 128 caractères."
+                    );
+            }
+
+            _name = name;
+        }
+
+        /// <summary>
+        /// Récupère le nom de la collection de sons sous forme de <see cref="string"/>
+        /// </summary>
+        /// <returns>Le nom de la collection de sons sous forme de <see cref="string"/></returns>
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
