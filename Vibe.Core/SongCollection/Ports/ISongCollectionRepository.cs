bcccb773b16bb5110a8aef865e29f70d84b15e14
Salomez Ethan <salomez-ethan@outlook.fr>
2026-02-06 11:17:14 +0100

    Ajout du repository

---
+++ Vibe.Core/SongCollection/Ports/ISongCollectionRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Vibe.Core.SongCollection.Ports
+{
+    public interface ISongCollectionRepository
+    {
+        Task<SongCollectionEntity> GetByIdAsync(Guid songCollectionId);
+        
+        Task AddAsync(SongCollectionEntity songCollection);
+        Task SaveAsync(SongCollectionEntity songCollection);
+        Task DeleteAsync(Guid songCollectionId);
+    }
+}
