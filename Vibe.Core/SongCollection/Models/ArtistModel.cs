6f2ffb397c61a824a935335656a94baecf4e0952
Salomez Ethan <salomez-ethan@outlook.fr>
2026-02-06 16:58:16 +0100

    SongCollection : Création des models pour les gateways

---
+++ Vibe.Core/SongCollection/Models/ArtistModel.cs
@@ -0,0 +1,4 @@
+﻿namespace Vibe.Core.SongCollection.Models
+{
+    public sealed record ArtistModel(Guid Id, string Name),
+}
---
+++ Vibe.Core/SongCollection/Models/SongModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Vibe.Core.SongCollection.Models
+{
+    public sealed record SongModel(
+        Guid Id, 
+        string Name, 
+        IEnumerable<ArtistModel> ArtistIds, 
+        TimeSpan Duration
+        );
+}
