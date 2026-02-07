7a49c13b7e62d23b888f55258aa4f2b393095a9e
Ethan Salomez <salomez-ethan@outlook.fr>
2026-02-07 14:33:50 +0100

    Renommage du model pour rester cohérent avec le reste du projet + ajout de la documentation.

--- Vibe.Core/SongCollection/Models/ArtistModel.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Vibe.Core.SongCollection.Models
-{
-    public sealed record ArtistModel(Guid Id, string Name);
-}
---
+++ Vibe.Core/SongCollection/Models/PerformerModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Vibe.Core.SongCollection.Models
+{
+    /// <summary>
+    /// Le model représentant un interprète.
+    /// </summary>
+    /// <param name="Id">L'identifiant de l'artiste.</param>
+    /// <param name="Name">Le nom de l'artiste.</param>
+    public sealed record PerformerModel(Guid Id, string Name);
+}
