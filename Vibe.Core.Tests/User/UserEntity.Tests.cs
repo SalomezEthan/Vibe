1d584bf279cd56ca8cdaedb0cd969755fa808f6b
Ethan Salomez <salomez-ethan@outlook.fr>
2026-02-07 14:44:49 +0100

    Ajout des tests (que j'ai oublié d'ajouter au dernier commit)

---
+++ Vibe.Core.Tests/User/UserEntity.Tests.cs
@@ -0,0 +1,31 @@
+﻿using Vibe.Core.User;
+
+namespace Vibe.Core.Tests.User
+{
+    [TestClass]
+    public sealed class UserEntityTests
+    {
+        private static UserEntity CreateUserEntitySample()
+        {
+            return new UserEntity(
+                [Guid.NewGuid()]
+                );
+        }
+
+        [TestMethod]
+        public void AddFavoriteSong_ShouldThrow_WhenFavoritesAlreadyContainsSong()
+        {
+            var user = CreateUserEntitySample();
+            Guid duplicatedId = user.FavoriteSongIds[0];
+            Assert.ThrowsExactly<ArgumentException>(() => user.AddFavoriteSong(duplicatedId));
+        }
+
+        [TestMethod]
+        public void RemoveFavoriteSong_ShouldThrow_WhenFavoriteNotContainsSong()
+        {
+            var user = CreateUserEntitySample();
+            var randomId = Guid.NewGuid();
+            Assert.ThrowsExactly<ArgumentException>(() => user.RemoveFavoriteSong(randomId));
+        }
+    }
+}
