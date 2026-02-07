0f3cf47c9edbf38e43aec6e51a80ab2cc32a61be
Ethan Salomez <salomez-ethan@outlook.fr>
2026-02-07 12:00:17 +0100

    Ajout de la passerelle entre Song et SongCollection

---
+++ Vibe.Core/SongCollection/Ports/ISongGateway.cs
@@ -0,0 +1,30 @@
+﻿using Vibe.Core.SongCollection.Models;
+
+namespace Vibe.Core.SongCollection.Ports
+{
+    /// <summary>
+    /// Permet de récupérer les sons d'une collection.
+    /// </summary>
+    public interface ISongGateway
+    {
+        /// <summary>
+        /// Récupère les sons d'une collection.
+        /// </summary>
+        /// <param name="songIds">La collections d'identidiants des sons.</param>
+        /// <returns>La collection.</returns>
+        Task<IReadOnlyList<SongModel>> GetSongsAsync(IEnumerable<Guid> songIds);
+
+        /// <summary>
+        /// Récupère tous les sons.
+        /// </summary>
+        /// <returns>Tous les sons.</returns>
+        Task<IReadOnlyList<SongModel>> GetAllSongsAsync();
+
+        /// <summary>
+        /// Récupère les sons favoris d'un utilisateur.
+        /// </summary>
+        /// <param name="userId">L'identifiant de l'utilisateur.</param>
+        /// <returns>Les sons favoris de l'utilisateur.</returns>
+        Task<IReadOnlyList<SongModel>> GetUserFavoriteSongsAsync(Guid userId);
+    }
+}
